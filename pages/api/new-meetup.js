969cca2dd2d5e3a003907a3b9f78961b855eaf8d
Uzzal Roy <32016943+uzzal71@users.noreply.github.com>
2023-04-01 12:15:32 +0600

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -8,7 +8,7 @@ async function handler(req, res) {
     const data = req.body;
     // const { title, image, address, description } = data;
 
-    const client = await MongoClient.connect('mongodb+srv://Uzzalroy_96:Uzzalroy_96@cluster0.ysa2z.mongodb.net/meetups?retryWrites=true&w=majority');
+    const client = await MongoClient.connect('mongodb://localhost:27017/meetups);
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
